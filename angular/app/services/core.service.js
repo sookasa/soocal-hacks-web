3e6d349cb1fcfa02df9d992d960829b6b8a0a4c3
Alon Regev <alon@sookasa.com>
2015-10-10 15:49:25 -0700

    Moved choices initialization to `coreService`

--- angular/app/services/core.service.js
+++ angular/app/services/core.service.js
@@ -4,6 +4,33 @@ angular.module('myApp.services', []).
         //var api = {};
         console.log('service');
         var user = {};
+//        var this.choices = {};
+        var initChoices = function () {
+            // get choices from server api
+            this.choices = [
+                {
+                    name: 'Pacific Catch',
+                    tags: ['asian'],
+                    picture: 'https://material.angularjs.org/0.11.2/img/icons/angular-logo.svg',
+                    yelp: 'http://www.yelp.com/pacific-catch'
+                },
+                {
+                    name: 'Seniores Pizza',
+                    tags: ['pizza'],
+                    picture: 'https://material.angularjs.org/0.11.2/img/icons/angular-logo.svg',
+                    yelp: 'http://www.yelp.com/seniores-pizza'
+                },
+                {
+                    name: 'B Street and Vine',
+                    tags: ['italian'],
+                    picture: 'https://material.angularjs.org/0.11.2/img/icons/angular-logo.svg',
+                    yelp: 'http://www.yelp.com/b-street-and-vine'
+                }
+            ];
+            for (choice in this.choices) {
+                choice.choice = false;
+            }
+        }
 
         return {
             setUser: function (user) {
@@ -12,6 +39,10 @@ angular.module('myApp.services', []).
 
             getUser: function (user) {
                 return this.user;
+            },
+
+            getChoices: function () {
+                this.initChoices();
             }
         }
 
