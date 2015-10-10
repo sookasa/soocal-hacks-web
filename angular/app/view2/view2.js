712490af5e2d722ebb08813190944a4e90692b3f
Nadia Korshun <nadia@sookasa.com>
2015-10-10 14:23:30 -0700

    redirect if no user

--- angular/app/view2/view2.js
+++ angular/app/view2/view2.js
@@ -2,16 +2,20 @@
 
 angular.module('myApp.view2', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view2', {
-    templateUrl: 'view2/view2.html',
-    controller: 'View2Ctrl'
-  });
-}])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/view2', {
+            templateUrl: 'view2/view2.html',
+            controller: 'View2Ctrl'
+        });
+    }])
 
-.controller('View2Ctrl', ['$scope','coreService', function($scope, coreService) {
-      var user = coreService.getUser();
-      console.log('user.email=' + user.email);
-      $scope.user = coreService.getUser();
+    .controller('View2Ctrl', ['$scope', '$location', 'coreService', function ($scope, $location, coreService) {
+        var user = coreService.getUser();
+        if (!user) {
+            console.log('wrong virw');
+            $location.path('/view1');
+        }
+        //console.log('user.email=' + user.email);
+        //$scope.user = coreService.getUser();
 
-}]);
+    }]);
--- angular/app/view3/view3.js
+++ angular/app/view3/view3.js
@@ -2,13 +2,16 @@
 
 angular.module('myApp.view3', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view3', {
-    templateUrl: 'view3/view3.html',
-    controller: 'View3Ctrl'
-  });
-}])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/view3', {
+            templateUrl: 'view3/view3.html',
+            controller: 'View3Ctrl'
+        });
+    }])
 
-.controller('View3Ctrl', [function() {
-
-}]);
+    .controller('View3Ctrl', ['$scope', '$location', 'coreService', function ($scope, $location, coreService) {
+        var user = coreService.getUser();
+        if (!user) {
+            $location.path('/view1');
+        }
+    }]);
