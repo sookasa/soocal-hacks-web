62a00f8448c117f21bbe40f5ce941239ef971f59
Schweigi <marco@mailrelay.ch>
2015-10-10 17:39:50 -0700

    Add default date for restaurants function

--- backend/src/restaurant_handler.js
+++ backend/src/restaurant_handler.js
@@ -3,7 +3,8 @@ var async = require('async');
 exports.restaurant_handler = function(event, context) {
     console.log('Restaurant Handler - Received Event:', JSON.stringify(event, null, 2));
 
-    var date = event.date;
+    var date = event.date !== undefined ? new Date(event.date): new Date();
+    date.setHours(0,0,0,0);
 
     if (date === undefined) {
         context.done({ "status": "error", "error": "Invalid data" });
